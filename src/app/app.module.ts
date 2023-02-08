df1f98cc45204697aa77c39e21e5a6121d1bc7c3
Michael Panches <michael.panchesi@ug.edu.ec>
2023-02-08 18:34:15 -0500

    Creación de Componentes, Módulos y Servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { ErrorComponent } from './componentes/error/error.component';
+import { SpinnerComponent } from './componentes/spinner/spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ErrorComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
